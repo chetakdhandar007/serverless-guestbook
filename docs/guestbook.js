98458a621e6989cb0b1e3d5bad48b0a9fc4019c4
chetakdhandar007 <40737756+chetakdhandar007@users.noreply.github.com>
2020-05-18 00:49:07 +0530

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud//cmatch';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
